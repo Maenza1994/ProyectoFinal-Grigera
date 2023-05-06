e455e21d3e0f48e8814602a67f63e418f72783c8
Maenza1994 <102321540+Maenza1994@users.noreply.github.com>
2023-05-06 00:25:00 -0300

    entrega final

--- JS/script.js
+++ JS/script.js
@@ -8,21 +8,11 @@ const btnFiltrar = document.querySelector("#btnFiltrar"),
     btnGuardarLocal = document.querySelector("#btnGuardarLocal"),
     btnRecuperarLocal = document.querySelector("#btnRecuperarLocal"),
     txtNombre = document.querySelector("#agregarNombre"),
-    txtPrecio = document.querySelector("#agregarPrecio");
+    txtPrecio = document.querySelector("#agregarPrecio"),
+    padreProductos = document.querySelector('.padreProductos'),
+    contenedor = document.querySelector("#losProductos");
+let productos, arrMostrarProductos = [];
 
-//Arrays
-const productos = [
-    { id: 1, nombre: "camara canon", precio: 165000, imagen: "camara canon" },
-    { id: 2, nombre: "camara nikon", precio: 135960, imagen: "camara nikon" },
-    { id: 3, nombre: "soporte canon", precio: 27055, imagen: "soporte canon" },
-    { id: 4, nombre: "soporte nikon", precio: 19000, imagen: "soporte nikon" },
-    { id: 5, nombre: "memoria sd", precio: 37000, imagen: "memoria sd" },
-    { id: 6, nombre: "baterias nikon", precio: 19000, imagen: "baterias nikon" },
-    { id: 7, nombre: "baterias sony", precio: 21000, imagen: "baterias sony" },
-    { id: 8, nombre: "camara sony", precio: 180650, imagen: "camara sony" },
-    { id: 9, nombre: "gimball", precio: 101000, imagen: "gimball" },
-],
-    arrMostrarProductos = [];
 
 //Clase producto con su respectivo constructor
 class Producto {
@@ -33,15 +23,52 @@ class Producto {
     }
 }
 
+
 //Load de la pagina
-llenarArrMostrarProductos(productos);
-mostrarProductos(arrMostrarProductos);
+cargarConFetch();
+
 
 //Funciones
-//Estructura del HTML
+function vaciarContenedor() {
+    contenedor.innerHTML = "";
+};
+
+function cargarConFetch() {
+    fetch('../data.json')
+        .then((res) => res.json())
+        .then((data) => {
+            productos = data;
+            data.forEach((producto) => {
+                //Creo un div llamado "contenedor"
+                let contenedor = document.createElement("div");
+                //Le agrego la clase (CSS)
+                contenedor.classList.add("tarjetaProducto");
+                //Defino el innerHTML del elemento con una plantilla de texto
+                contenedor.innerHTML = `<div class="tarjetaProducto">
+                                <div class="imagenProducto">
+                                    <img src="../images/${producto.imagen}.jpg" alt=${producto.nombre}>
+                                </div>
+                                <div class="descripcionProducto">
+                                    <div class="nombreProducto">
+                                        <h2 class="descFoto">${producto.nombre}</h2>
+                                    </div>
+                                    <div class="precioProducto">
+                                        <h2 class="descFoto">$${producto.precio}</h2>
+                                    </div>
+                                </div>
+                            </div>  `;
+                padreProductos.append(contenedor)
+            })
+        })
+}
+
+function llenarArr(arr, arrAllenar) {
+    for (let i = 0; i < arr.length; i++) {
+        arrAllenar.push(arr[i]);
+    }
+};
+
 function mostrarProductos(arr) {
-    //Asigno a la variable "padreProductos" los div cuya clase sea ="padreProductos"
-    const padreProductos = document.querySelector('.padreProductos');
     //Recorro el array productos, y para cada objeto:
     for (const producto of arr) {
         //Creo un div llamado "contenedor"
@@ -61,20 +88,11 @@ function mostrarProductos(arr) {
                                         <h2 class="descFoto">$${producto.precio}</h2>
                                     </div>
                                 </div>
-                                <div class="areaBotonProducto">
-                                    <button type="button" class="botonProducto" onclick="apretarBoton()">Agregar Producto</button>
-                                </div>
                             </div>  `;
         //Anexo a la variable (div con clase productos) la estructura creada (con la clase ya incluida)
         padreProductos.appendChild(contenedor);
     };
 }
-
-function llenarArrMostrarProductos(arr) {
-    for (let i = 0; i < arr.length; i++) {
-        arrMostrarProductos.push(arr[i]);
-    }
-};
 
 function filtrarNombre(arr, filtro) {
     //Las variables de los input no las reconocen si estan declaradas como globales
@@ -90,13 +108,6 @@ function vaciarArray(arr) {
     arr.splice(0)
 };
 
-function vaciarProductos() {
-    let productos = document.getElementsByClassName("tarjetaProducto");
-    for (let i = 0; i < productos.length; i++) {
-        productos[i].remove()
-    }
-};
-
 function agregarProducto() {
     const nom = txtNombre.value;
     const pre = parseFloat(txtPrecio.value);
@@ -105,48 +116,91 @@ function agregarProducto() {
     productos.push({ id: idNuevo, nombre: nom, precio: pre, imagen: image });
 };
 
-function guardarLocal() {
-    const guardarLocal = (clave, valor) => { localStorage.setItem(clave, valor) };
-    //Almacenar producto por producto
-    for (const producto of productos) {
-        guardarLocal(producto.id, JSON.stringify(producto));
-    }
-    // o almacenar array completo
-    guardarLocal("listaProductos", JSON.stringify(productos));
+function toast(Mensaje) {
+    Toastify({
+        text: Mensaje,
+        duration: 2000,
+        close: true,
+        gravity: "botom",
+        position: "right",
+        stopOnFocus: true,
+        style: {
+            background: "linear-gradient(to right, #00b09b, #96c93d)",
+        },
+    }).showToast();
 };
 
-function recuperarLocal() {
-    //Vacio el array "arrMostrarProductos"
-    vaciarArray(arrMostrarProductos)
-    //Obtengo el listado de productos almacenado en JSON ya transformado en objetos
-    const almacenados = JSON.parse(localStorage.getItem("listaProductos"));
-    //lleno el ArrMostrarProductos con el array almacenado
-    llenarArrMostrarProductos(almacenados);
-    vaciarProductos();
-    mostrarProductos(arrMostrarProductos);
-};
 
 //Eventos
 btnFiltrar.addEventListener("click", () => {
-    const filtradoNombre = filtrarNombre(arrMostrarProductos, filtroNombre.value);
-    vaciarProductos();
+    vaciarContenedor();
     vaciarArray(arrMostrarProductos);
-    llenarArrMostrarProductos(filtradoNombre);
+    llenarArr(productos, arrMostrarProductos);
+    const filtradoNombre = filtrarNombre(arrMostrarProductos, filtroNombre.value);
+    vaciarArray(arrMostrarProductos);
+    llenarArr(filtradoNombre, arrMostrarProductos);
     mostrarProductos(arrMostrarProductos);
+    vaciarArray(arrMostrarProductos);
 });
 
 btnAgregar.addEventListener("click", () => {
-    vaciarProductos();
+    vaciarContenedor();
     agregarProducto();
     vaciarArray(arrMostrarProductos);
-    llenarArrMostrarProductos(productos);
+    llenarArr(productos, arrMostrarProductos);
     mostrarProductos(arrMostrarProductos);
+    document.querySelector("#agregarNombre").value = "";
+    document.querySelector("#agregarPrecio").value = "";
+    toast("Agregado Correctamente");
 });
 
 btnGuardarLocal.addEventListener("click", () => {
-    guardarLocal();
+    Swal.fire({
+        title: "Acción no reversible",
+        text: "¿Está seguro de querer guardar los productos actuales en almacenamiento local? Se sobrescribiran los datos guardados actualmente",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Confirmar",
+        cancelButtonText: "Cancelar",
+    }).then((result) => {
+        if (result.isConfirmed) {
+        const guardarLocal = (clave, valor) => { localStorage.setItem(clave, valor) };
+        //Almacenar producto por producto
+        for (const producto of productos) {
+            guardarLocal(producto.id, JSON.stringify(producto));
+        }
+        // o almacenar array completo
+        guardarLocal("listaProductos", JSON.stringify(productos));
+        Swal.fire("Exito!", "Los productos se han guardado.", "success");
+    }
+    });
 });
 
 btnRecuperarLocal.addEventListener("click", () => {
-    recuperarLocal();
+    Swal.fire({
+        title: "Acción no reversible",
+        text: "¿Está seguro de querer recuperar los productos almacenados en almacenamiento local? Se perderán los datos no guardados",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Confirmar",
+        cancelButtonText: "Cancelar",
+    }).then((result) => {
+        if (result.isConfirmed) {
+            vaciarContenedor();
+            //Vacio el array "arrMostrarProductos"
+            vaciarArray(arrMostrarProductos)
+            vaciarArray(productos)
+            //Obtengo el listado de productos almacenado en JSON ya transformado en objetos
+            const almacenados = JSON.parse(localStorage.getItem("listaProductos"));
+            //lleno los Array locales con el array almacenado
+            llenarArr(almacenados, productos)
+            llenarArr(almacenados, arrMostrarProductos);
+            mostrarProductos(arrMostrarProductos);
+            Swal.fire("Exito!", "Los productos se han recuperado.", "success");
+        }
+    });
 });
